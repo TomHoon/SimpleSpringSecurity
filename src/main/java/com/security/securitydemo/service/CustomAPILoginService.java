40f0d1c18b653a991f30affe05c41e1d6071474b
tomhoon <gnsdl9108@naver.com>
2025-04-26 07:41:16 +0900

    feat: kakao 셋팅 전

--- src/main/java/com/security/securitydemo/service/CustomAPILoginService.java
+++ src/main/java/com/security/securitydemo/service/CustomAPILoginService.java
@@ -1,20 +1,29 @@
 package com.security.securitydemo.service;
 
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
-public class CustomAPILoginService implements UserDetailsService{
+@RequiredArgsConstructor
+public class CustomAPILoginService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        System.out.println(username);
+        String encoded = passwordEncoder.encode("1234");
 
-        throw new UsernameNotFoundException("test");
-
+        return new User(username, encoded, List.of(new SimpleGrantedAuthority("ROLE_USER")));
     }
-    
+
 }
